c4070ccf5962808384398eac07df5eff1f989d18
Phan Thế Tuệ <68698678+phanthetue2309@users.noreply.github.com>
2023-01-26 12:35:12 +0700

    set up again

--- QLVLXD/vatlieu/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VatlieuConfig(AppConfig):
-    name = 'vatlieu'
---
+++ building_material/bm_material/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VatlieuConfig(AppConfig):
+    name = 'bm_material'
